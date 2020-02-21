2d2dc84ff5d0a3398b26b3d9e6d2890ed8925eef
vinicius <vpereirao@eservicios.indracompany.com>
2020-02-21 16:59:33 +0000

    first commit

---
+++ src/main/java/com/streams/examples/mystreamsexamples/MyStreamsExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.streams.examples.mystreamsexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStreamsExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStreamsExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streams/examples/mystreamsexamples/MyStreamsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streams.examples.mystreamsexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStreamsExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
